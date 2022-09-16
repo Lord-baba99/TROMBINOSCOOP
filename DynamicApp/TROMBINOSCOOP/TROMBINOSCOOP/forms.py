8e0afbdf02d8aab12a8cecb0b7219fb3c070b237
Lord-baba99 <prcl.baba@gmail.com>
2022-09-16 13:52:48 +0200

    Second commit

--- DynamicApp/TROMBINOSCOOP/TROMBINOSCOOP/forms.py
+++ DynamicApp/TROMBINOSCOOP/TROMBINOSCOOP/forms.py
@@ -4,3 +4,14 @@ from django import forms
 class LoginForm(forms.Form):
     email = forms.EmailField(label='Courriel')
     password = forms.CharField(label='Mot de passe', widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super(LoginForm, self).clean()
+        email = cleaned_data.get("email")
+        password = cleaned_data.get("password")
+
+        # verifie que les deux champs sont valides
+        if password and email:
+            if password != 'alk' and email != 'mail@domain.com':
+                raise forms.ValidationError("Adresse de couriel ou mot de passe erroné.")
+        return cleaned_data
